861efcbc26b735be7127c278233af393d5166b3f
danielhabila <100402238+danielhabila@users.noreply.github.com>
2023-04-08 16:32:17 -0700

    #

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import axios from "axios";
 
-if (process.env.NODE_ENV === "development") {
-  axios.defaults.baseURL = "http://localhost:4040";
-}
+// if (process.env.NODE_ENV === "development") {
+//   axios.defaults.baseURL = "http://localhost:4040";
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
